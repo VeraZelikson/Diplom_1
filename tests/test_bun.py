8d5e45344ae53c0210c8dd277589cfe22c709cf2
Vera Zelikson <venzx@yandex.ru>
2024-10-25 20:06:59 +0300

    Append tests for bun

---
+++ tests/test_bun.py
@@ -0,0 +1,11 @@
+from bun import Bun
+
+
+class TestBun:
+    def test_get_name(self):
+        bun = Bun('TestBun', 1)
+        assert bun.get_name() == 'TestBun'
+
+    def test_get_price(self):
+        bun = Bun('TestBun', 2.7)
+        assert bun.get_price() == 2.7
